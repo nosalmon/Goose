1bfbdf745f27b56d072c49037b7069fa88cc76db
Yisuk <kimystd@gmail.com>
2019-03-21 22:25:44 +0900

    Apply animation for horizontal movemnet

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigid;
+    private PlayerAnimation _playerAnimation;
+    private SpriteRenderer _spriteRenderer;
+
+    private float speedFactor = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+        _playerAnimation = GetComponent<PlayerAnimation>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        if (horizontalInput < 0)
+        {
+            Flip(true);
+        }
+        else if (horizontalInput > 0)
+        {
+            Flip(false);
+        }
+        _playerAnimation.SideWalk(horizontalInput);
+        _rigid.velocity = new Vector2(horizontalInput * speedFactor, verticalInput * speedFactor);
+
+    }
+
+    void Flip(bool isHeadingLeft)
+    {
+        _spriteRenderer.flipX = !isHeadingLeft;
+    }
+}
---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SideWalk(float move)
+    {
+        _animator.SetFloat("Move", Mathf.Abs(move)); 
+    }
+}
